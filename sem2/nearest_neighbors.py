7924839c8928eece6ffd6937990c76782f7d95e2
aflock <andrewbflockhart@gmail.com>
2012-01-29 20:23:10 -0500

    psuedo

--- sem2/nearest_neighbors.py
+++ sem2/nearest_neighbors.py
@@ -16,6 +16,20 @@ Description: Nearest neighbors algorithm for classification of Cosmic Ray pixels
                 (<tuple or list of pixel values for the sample> , <1/0> ),
                 (<tuple or list of pixel values for the sample> , <1/0> ),
             ]
+
+Pseudo code:
+    training set = 1/2
+    test set = 5/12
+    validation set = 1/12
+
+
+
+    for k in [1,3,5,7,9]:
+        for sample, answer in VALIDATION set:
+            Get k nearest neighbors from training set = NN
+            guess = NN.vote()
+            if guess == answer :
+
 '''
 
 
