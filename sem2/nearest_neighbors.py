dcea3aa4584305788a00eb6f6794fd143cd1127b
aflock <andrewbflockhart@gmail.com>
2012-01-29 21:49:59 -0500

    flesh out these fools

--- sem2/nearest_neighbors.py
+++ sem2/nearest_neighbors.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+#imports
+import sys, os
+import numpy as np
+import pickle
+
+# TODO: fill this in plz VV (01/29/12, 21:42, AFlock)
+DATA_FILE = "./data.p"
 '''
 File: nearest_neighbors.py
 Author: AFlock
@@ -29,7 +36,30 @@ Pseudo code:
             Get k nearest neighbors from training set = NN
             guess = NN.vote()
             if guess == answer :
+                right[k] += 1
 
+
+Nearest Neighbors(sample, k):
+    for p in training set:
+        d = euclidean_dist(sample, p)
+        if d < kth best d:
+            update kth best
+    return kbest
 '''
+def main():
+    """runs nearest neighbors """
+    #get data
+    samples = pickle.load("DATA_FILE")
 
+    pass
 
+def nearest_neighbors(sample, k):
+    """Finds k nearest neighbors based on Euclidean distance
+
+    :sample: the sample
+    :k: how many neighbors to find
+    :returns: the neighbors themselves
+        (since we may want to show them, we will do the voting in main)
+    """
+
+    pass
